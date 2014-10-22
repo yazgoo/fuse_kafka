d2322bce6b106a472566d8b3f58de916c5d90eda
yazgoo <yazgoo@users.noreply.github.com>
2014-10-22 14:18:07 +0200

    updating setup script

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The following should install the new repositories then install fuse\_kafka:
 
     # curl -O \
         https://raw.githubusercontent.com/yazgoo/fuse_kafka/master/setup_packages.sh \
-        && md5sum -c <(echo "c0f7ecaefb7b3b0d3478d7099f25b6cd  setup_packages.sh") \
+        && md5sum -c <(echo "a9c81807fa1f84018f90bbd80e987863  setup_packages.sh") \
         && chmod +x setup_packages.sh && ./setup_packages.sh
 
 Quickstart (from sources)
--- setup_packages.sh
+++ setup_packages.sh
@@ -1,19 +1,20 @@
+# /!\ when modifying this file don't forget to update checksum in README.md
 # helper script to install fuse_kafka from OBS packages
 set -x
 distro_version=$(lsb_release -s -r)
 distro_name=$(lsb_release -s -i)
-add_yum_repos() {
-    echo \
-    "[$1]
-    name=$1's repo
-    baseurl=http://download.opensuse.org/repositories/home\:/$1/$2
-    gpgcheck=0
-    enabled=1" > /etc/yum.repos.d/$name.repo
+add_yum_repo() {
+    cat  > /etc/yum.repos.d/$1.repo << EOF
+[$1]
+name=$1's repo
+baseurl=http://download.opensuse.org/repositories/home\:/$1/$2
+gpgcheck=0
+enabled=1
+EOF
 }
 install_CentOS() {
-    for who in edenhill yazgoo
-    do add_yum_repo $who $distro_name-6
-    done
+    add_yum_repo edenhill ${distro_name}_$distro_name-6
+    add_yum_repo yazgoo $distro_name-6
     [ "$distro_version" = "6.3" ] && yum -y update glibc
     yum -y install fuse_kafka
 }
