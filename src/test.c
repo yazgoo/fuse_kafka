26e00d8504063303c764998652d2c229f2b93cd8
yazgoo <yazgoo@users.noreply.github.com>
2014-11-12 14:03:12 +0100

    travis

--- src/test.c
+++ src/test.c
@@ -285,7 +285,7 @@ static char* all_tests()
 int main(int argc, char** argv)
 {
     char* result = all_tests();
-    if (result != 0) printf("%s\n", result);
+    if (result != 0) printf("ASSERTION FAILED:\n%s\n", result);
     else printf("ALL TESTS PASSED\n");
     printf("Tests run: %d\n", tests_run);
     system("rm -f " TEST "/to");
