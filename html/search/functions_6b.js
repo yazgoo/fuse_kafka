06bd459961563422343c416266aaa77ec34a6c45
yazgoo <none@none.com>
2014-12-18 11:57:50 +0000

    auto updating documenation

--- html/search/functions_6b.js
+++ html/search/functions_6b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['kafka_5fstart',['kafka_start',['../classbuild_1_1TestMininet.html#abea6fef76c2d624f8ab4f9834fa30825',1,'build::TestMininet']]],
+  ['kafka_5fstop',['kafka_stop',['../classbuild_1_1TestMininet.html#a98246d1c87247c0a177700aaf62b9bf8',1,'build::TestMininet']]],
   ['kafka_5fwrite',['kafka_write',['../fuse__kafka_8c.html#a48ba4bf8f069d68a01fb5ec60ed8ce6c',1,'fuse_kafka.c']]]
 ];
--- html/search/functions_7a.js
+++ html/search/functions_7a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['zookeeper_5fstart',['zookeeper_start',['../classbuild_1_1TestMininet.html#aa3eddff2c619b28456be8a68dfe0c8cd',1,'build::TestMininet']]]
+  ['zookeeper_5fstart',['zookeeper_start',['../classbuild_1_1TestMininet.html#aa3eddff2c619b28456be8a68dfe0c8cd',1,'build::TestMininet']]],
+  ['zookeeper_5fstop',['zookeeper_stop',['../classbuild_1_1TestMininet.html#a1fbdfc51881cb089360d8fae89f47430',1,'build::TestMininet']]]
 ];
