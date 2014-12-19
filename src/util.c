243c3b638dae809e21008766b7886a41ea69b7ee
yazgoo <yazgoo@github.com>
2014-12-19 16:02:25 +0100

    shippable debug

--- src/util.c
+++ src/util.c
@@ -55,6 +55,7 @@ static char* get_command_line(int pid)
     {
         while((c = fgetc(f)) != EOF)
         {
+            printf("shippable debug: %c\n", c);
             if(c == 0) c = ' ';
             if(i >= (size - 1))
             {
