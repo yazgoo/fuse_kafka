1adab9c4e8529f98caff937633ee15feb857fbc5
yazgoo <yazgoo@github.com>
2015-04-26 16:41:36 +0200

    enhancing plugin doc

--- src/plugins/input/example.c
+++ src/plugins/input/example.c
@@ -2,9 +2,6 @@
 #include <unistd.h>
 int input_setup(int argc, char** argv, void* conf)
 {
-    do
-    {
-        output_write("example", "ping", strlen("ping"), 0);
-    }
+    do output_write("example", "ping", strlen("ping"), 0);
     while(sleep(1) >= 0);
 }
