480bbe1d0c4365f21ef03e6f9cb8920f3555c91b
yazgoo <yazgoo@github.com>
2015-06-17 23:17:50 +0200

    fixing tests

--- src/minunit.c
+++ src/minunit.c
@@ -11,21 +11,9 @@ void line()
     for(i = 0; i < w.ws_col; i++) printf("=");
     printf("\n");
 }
-void backtrace_crash(int sig) {
-    void *array[10];
-    size_t size;
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(-11);
-}
 int main(int argc, char** argv)
 {
     time_t start,end;
-    if(getenv("BACKTRACE_CRASH"))
-        signal(SIGSEGV, backtrace_crash);
     start=clock();
     line();
     char* result = all_tests();
